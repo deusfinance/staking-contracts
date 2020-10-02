cb6c7209d2ba6bb557ff77880f90f1d9ccdd579d
developer <developer@b>
2020-10-02 17:09:23 +0330

    add pendingReward, deposit and withdraw function

--- staking.sol
+++ staking.sol
@@ -4,35 +4,96 @@ pragma solidity 0.6.12;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
 
-interface IUniswapV2Pair {
+interface StakedToken {
 	function totalSupply() external view returns (uint);
 }
 
+interface RewardToken {
+
+}
+
 contract staking{
-	using SafeMath for uint256;
-	
-	uint256 public accSushiPerShare = 0;
-	uint256 public lastRewardBlock;
-	uint256 public sushiPerBlock = 100 * 10**18;
-	
-	IUniswapV2Pair uniswapPair = IUniswapV2Pair(0x50e37bB824Eba26669a4A5382A53fd2e034a6D4B);
-	
-	constructor () public {
-		lastRewardBlock = block.number;
+
+	struct UserData {
+		uint256 depositAmount;
+		uint256 paidReward;
 	}
 
+	using SafeMath for uint256;
+	
+
+	mapping (address => UserData) public users;
+
+	uint256 public rewardTillNowPerShare = 0;
+	uint256 public lastRewardedBlock;
+	uint256 public rewardPerBlock = 100 * 10**18;
+
+	StakedToken public stakedToken;
+	RewardToken public rewardToken;
+
+    event Deposit(address user, uint256 amount);
+    event Withdraw(address user, uint256 amount);
+    event EmergencyWithdraw(address user, uint256 amount);
+
+	constructor (address _stakedToken, address _rewardToken) public {
+		stakedToken = StakedToken(_stakedToken);
+		rewardToken = RewardToken(_rewardToken)
+		lastRewardedBlock = block.number;
+	}
 
 	function update() public {
-		if (block.number <= lastRewardBlock) {
+		if (block.number <= lastRewardedBlock) {
 			return;
 		}
-		uint256 lpSupply = uniswapPair.totalSupply();
+		// uint256 lpSupply = stakedToken.totalSupply();
+		uint256 rewardAmount = (block.number - lastRewardedBlock).mul(rewardPerBlock);
 		
-		uint256 multiplier = block.number - lastRewardBlock;
-		uint256 sushiReward = multiplier.mul(sushiPerBlock);
+		rewardTillNowPerShare = rewardTillNowPerShare.add(rewardAmount.mul(1e18).div(stakedToken.totalSupply()));
+		lastRewardedBlock = block.number;
+    }
+
+	function pendingReward(address _user) external view returns (uint256) {
+		UserData storage user = users[_user];
+		uint256 accRewardPerShare = rewardTillNowPerShare;
+
+		if (block.number > lastRewardedBlock) {
+			uint256 rewardAmount = (block.number - lastRewardedBlock).mul(rewardPerBlock);
+            accRewardPerShare = accRewardPerShare.add(rewardAmount.mul(1e18).div(stakedToken.totalSupply()));
+        }
+        return user.depositAmount.mul(accRewardPerShare).div(1e18).sub(user.paidReward);
+	}
+
+	function deposit(uint256 _amount) public {
+		UserData storage user = users[msg.sender];
+        update();
+
+        if (user.amount > 0) {
+            uint256 pending = user.depositAmount.mul(rewardTillNowPerShare).div(1e18).sub(user.paidReward);
+        	safeSushiTransfer(msg.sender, pending);
+        }
+		// stakedToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+		safeTransferFrom(address(msg.sender), address(this), _amount);
+
+		user.depositAmount = user.depositAmount.add(_amount);
+        user.paidReward = user.depositAmount.mul(rewardTillNowPerShare).div(1e18);
+        emit Deposit(msg.sender, _amount);
+    }
+
+	function withdraw(uint256 _amount) public {
+		UserData storage user = users[msg.sender];
+        require(user.depositAmount >= _amount, "withdraw amount exceeds deposited amount");
+        update();
 		
-		accSushiPerShare = accSushiPerShare.add(sushiReward.mul(1e12).div(lpSupply));
-		lastRewardBlock = block.number;
+		uint256 pending = user.depositAmount.mul(rewardTillNowPerShare).div(1e18).sub(user.paidReward);
+		safeSushiTransfer(msg.sender, pending);
+
+		user.depositAmount = user.depositAmount.sub(_amount);
+
+		// stakedToken.safeTransfer(address(msg.sender), _amount);
+		safeTransfer(address(msg.sender), _amount);
+
+        user.paidReward = user.depositAmount.mul(rewardTillNowPerShare).div(1e18);
+        emit Withdraw(msg.sender, _amount);
     }
 
 }
