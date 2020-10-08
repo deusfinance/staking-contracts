fc7841726d6b21c142989edb6855a1a7a3f83b27
developer <developer@b>
2020-10-08 03:50:20 +0330

    fix approve on rebase bug.

--- DEAToken.sol
+++ DEAToken.sol
@@ -66,7 +66,10 @@ contract DEAToken is ERC20, AccessControl{
 
 
 	function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
-        return super.transferFrom(sender, recipient, amount.mul(rebaseMultiplierScale).div(rebaseMultiplier));
+        // return super.transferFrom(sender, recipient, amount.mul(rebaseMultiplierScale).div(rebaseMultiplier));
+		_transfer(sender, recipient, amount.mul(rebaseMultiplierScale).div(rebaseMultiplier));
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        return true;
     }
 
 	function transfer(address recipient, uint256 amount) public override returns (bool) {
